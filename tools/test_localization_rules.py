c950a2d70c06e963729852a9f3e9fe55d12b4309
Argone <52884093+crvstaldreams@users.noreply.github.com>
2026-09-18 13:34:37 +0300

    Add regression tests for Ukrainian aviation terms

---
+++ tools/test_localization_rules.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import unittest
+
+from generate_ukrainian_translations import contextual_override
+
+
+class UkrainianLocalizationRulesTest(unittest.TestCase):
+    def test_airframe_frame_is_rama(self):
+        self.assertEqual(
+            contextual_override(
+                "Frame",
+                "APMAirframeComponent",
+                ["../src/AutoPilotPlugins/APM/APMAirframeComponent.qml"],
+            ),
+            "Рама",
+        )
+
+    def test_airframe_frame_type_is_typ_ramy(self):
+        self.assertEqual(
+            contextual_override(
+                "Frame Type",
+                "APMAirframeComponent",
+                ["../src/AutoPilotPlugins/APM/APMAirframeComponent.qml"],
+            ),
+            "Тип рами",
+        )
+
+    def test_video_frame_is_kadr(self):
+        self.assertEqual(
+            contextual_override(
+                "Frame",
+                "VideoSettings",
+                ["../src/VideoManager/VideoSettings.qml"],
+            ),
+            "Кадр",
+        )
+
+    def test_attitude_terms_use_aviation_vocabulary(self):
+        location = ["../src/FlightMap/Widgets/QGCAttitudeWidget.qml"]
+        self.assertEqual(contextual_override("Roll", "Attitude", location), "Крен")
+        self.assertEqual(contextual_override("Pitch", "Attitude", location), "Тангаж")
+        self.assertEqual(contextual_override("Yaw", "Attitude", location), "Рискання")
+
+    def test_terrain_frame_is_coordinate_frame(self):
+        self.assertEqual(
+            contextual_override(
+                "Terrain Frame",
+                "MissionItem",
+                ["../src/MissionManager/MissionItem.qml"],
+            ),
+            "Система координат рельєфу",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
