4dbbcc7a88877d58cc9d4c68890a0de22b095242
Argone <52884093+crvstaldreams@users.noreply.github.com>
2026-09-23 19:09:29 +0300

    Calculate independent magnetic field magnitude

--- custom/src/CompassTelemetryController.cc
+++ custom/src/CompassTelemetryController.cc
@@ -11,16 +11,17 @@ void CompassTelemetryController::setVehicle(Vehicle* vehicle) {
     if (_vehicle) connect(_vehicle, &Vehicle::mavlinkMessageReceived, this, &CompassTelemetryController::_message);
     emit vehicleChanged(); emit headingsChanged();
 }
-void CompassTelemetryController::_set(int i,double x,double y) {
-    if (qFuzzyIsNull(x) && qFuzzyIsNull(y)) return;
+void CompassTelemetryController::_set(int i,double x,double y,double z) {
+    if (qFuzzyIsNull(x) && qFuzzyIsNull(y) && qFuzzyIsNull(z)) return;
     double d=qRadiansToDegrees(qAtan2(y,x)); if(d<0)d+=360.0;
+    _x[i]=x; _y[i]=y; _z[i]=z; _field[i]=qSqrt(x*x+y*y+z*z);
     _heading[i]=d; _valid[i]=true; emit headingsChanged();
 }
 void CompassTelemetryController::_message(const mavlink_message_t& m) {
     switch(m.msgid) {
-    case MAVLINK_MSG_ID_RAW_IMU: { mavlink_raw_imu_t v; mavlink_msg_raw_imu_decode(&m,&v); _set(0,v.xmag,v.ymag); break; }
-    case MAVLINK_MSG_ID_SCALED_IMU2: { mavlink_scaled_imu2_t v; mavlink_msg_scaled_imu2_decode(&m,&v); _set(1,v.xmag,v.ymag); break; }
-    case MAVLINK_MSG_ID_SCALED_IMU3: { mavlink_scaled_imu3_t v; mavlink_msg_scaled_imu3_decode(&m,&v); _set(2,v.xmag,v.ymag); break; }
+    case MAVLINK_MSG_ID_RAW_IMU: { mavlink_raw_imu_t v; mavlink_msg_raw_imu_decode(&m,&v); _set(0,v.xmag,v.ymag,v.zmag); break; }
+    case MAVLINK_MSG_ID_SCALED_IMU2: { mavlink_scaled_imu2_t v; mavlink_msg_scaled_imu2_decode(&m,&v); _set(1,v.xmag,v.ymag,v.zmag); break; }
+    case MAVLINK_MSG_ID_SCALED_IMU3: { mavlink_scaled_imu3_t v; mavlink_msg_scaled_imu3_decode(&m,&v); _set(2,v.xmag,v.ymag,v.zmag); break; }
     default: break;
     }
 }
