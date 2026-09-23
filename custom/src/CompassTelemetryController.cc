7cdf81203f2983c105602097c95bfa068be5bc15
Argone <52884093+crvstaldreams@users.noreply.github.com>
2026-09-23 19:02:31 +0300

    Decode independent MAVLink magnetometer streams

---
+++ custom/src/CompassTelemetryController.cc
@@ -0,0 +1,26 @@
+#include "CompassTelemetryController.h"
+#include "Vehicle.h"
+#include <QtMath>
+
+CompassTelemetryController::CompassTelemetryController(QObject* parent):QObject(parent){}
+
+void CompassTelemetryController::setVehicle(Vehicle* vehicle) {
+    if (_vehicle == vehicle) return;
+    if (_vehicle) disconnect(_vehicle, nullptr, this, nullptr);
+    _vehicle=vehicle; _valid={{false,false,false}};
+    if (_vehicle) connect(_vehicle, &Vehicle::mavlinkMessageReceived, this, &CompassTelemetryController::_message);
+    emit vehicleChanged(); emit headingsChanged();
+}
+void CompassTelemetryController::_set(int i,double x,double y) {
+    if (qFuzzyIsNull(x) && qFuzzyIsNull(y)) return;
+    double d=qRadiansToDegrees(qAtan2(y,x)); if(d<0)d+=360.0;
+    _heading[i]=d; _valid[i]=true; emit headingsChanged();
+}
+void CompassTelemetryController::_message(const mavlink_message_t& m) {
+    switch(m.msgid) {
+    case MAVLINK_MSG_ID_RAW_IMU: { mavlink_raw_imu_t v; mavlink_msg_raw_imu_decode(&m,&v); _set(0,v.xmag,v.ymag); break; }
+    case MAVLINK_MSG_ID_SCALED_IMU2: { mavlink_scaled_imu2_t v; mavlink_msg_scaled_imu2_decode(&m,&v); _set(1,v.xmag,v.ymag); break; }
+    case MAVLINK_MSG_ID_SCALED_IMU3: { mavlink_scaled_imu3_t v; mavlink_msg_scaled_imu3_decode(&m,&v); _set(2,v.xmag,v.ymag); break; }
+    default: break;
+    }
+}
